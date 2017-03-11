9917d76b34af7144b42587dd26516f089be8b443
navigatron <roboticstranger@gmail.com>
2017-03-11 18:06:38 -0500

    %1

--- php/ConnectToDatabase.php
+++ php/ConnectToDatabase.php
@@ -3,8 +3,9 @@
 $server='127.0.0.1';
 $username='root';
 $password='mysjavatech98';
+$database='memestreme';
 //Create the Connection;
-$DatabaseConnection = new mysqli($server, $username, $password);
+$DatabaseConnection = new mysqli($server, $username, $password, $database);
 //Make sure no connection errors
 if($DatabaseConnection->connect_error){
     die("Connection Error: ".$DatabaseConnection->connect_error);
