56213ed8f46aed6359b5bcb92e17b5efd975c042
navigatron <roboticstranger@gmail.com>
2017-03-11 17:24:25 -0500

    %1

--- test.php
+++ test.php
@@ -4,12 +4,13 @@ echo "Welcome to the Test!";
 $server='127.0.0.1';
 $username='root';
 $password='mysjavatech98';
-
+echo "Connecting...";
 //Create the Connection;
 $DatabaseConnection = new mysqli($server, $username, $password);
-
+echo "Connected!";
 //Make sure no connection errors
 if($DatabaseConnection->connect_error){
+    echo "Database Error: ".$DatabaseConnection->connect_error;
     die("Connection Error: ".$DatabaseConnection->connect_error);
 }
 echo "Connected to the database!";
