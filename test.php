088b4b164577a19c6768364d77f57361e24d51f8
navigatron <roboticstranger@gmail.com>
2017-03-11 18:00:30 -0500

    %1

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);
-require_once(dirname(__DIR__)."/html/php/ReportErrors.php");
-require_once(dirname(__DIR__)."/html/php/ConnectToDatabase.php");
-echo "All systems Go";
+define('ROOT', dirname(__DIR__).'/html/php/');
+require_once(ROOT.'ReportErrors.php');
+require_once(ROOT.'ConnectToDatabase.php');
+echo 'All systems Go';
