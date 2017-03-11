322142947662de3b74267c59115860c36cf36e0b
navigatron <roboticstranger@gmail.com>
2017-03-11 17:56:09 -0500

    %1

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
-require_once(dirname(__DIR__)."/php/ReportErrors.php");
-require_once(dirname(__DIR__)."/php/ConnectToDatabase.php");
+require_once(dirname(__DIR__)."/html/php/ReportErrors.php");
+require_once(dirname(__DIR__)."/html/php/ConnectToDatabase.php");
 echo "All systems Go";
