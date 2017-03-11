a036c7dcb037a5f012d3d2d053f18b427d977e71
navigatron <roboticstranger@gmail.com>
2017-03-11 17:49:33 -0500

    %1

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
 require_once("php/ReportErrors.php");
 require_once("php/ConnectToDatabase.php");
 echo "All systems Go";
