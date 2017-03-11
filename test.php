bfdb523e13d65f1d00d07e034aba8745564cf59c
navigatron <roboticstranger@gmail.com>
2017-03-11 18:01:25 -0500

    %1

--- test.php
+++ test.php
@@ -3,3 +3,4 @@ define('ROOT', dirname(__DIR__).'/html/php/');
 require_once(ROOT.'ReportErrors.php');
 require_once(ROOT.'ConnectToDatabase.php');
 echo 'All systems Go';
+$DatabaseConnection->close();
