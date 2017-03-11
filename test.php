a0ab150a98bb2dec29a6fa77fb729b8cdbae4560
navigatron <roboticstranger@gmail.com>
2017-03-11 18:05:16 -0500

    %1

--- test.php
+++ test.php
@@ -3,4 +3,17 @@ define('ROOT', dirname(__DIR__).'/html/php/');
 require_once(ROOT.'ReportErrors.php');
 require_once(ROOT.'ConnectToDatabase.php');
 echo 'All systems Go';
+
+$sql = "SELECT * FROM test";
+$result = $DatabaseConnection->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: ".$row["id"].", text: ".$row["text"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+
 $DatabaseConnection->close();
