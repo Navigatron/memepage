ca2743f78c4f74f43b09e896f9f436e3e0a47a4e
navigatron <roboticstranger@gmail.com>
2017-03-12 17:20:40 -0400

    Got a php linter! Yeeee

--- github.php
+++ github.php
@@ -11,38 +11,38 @@
         echo 'Including error reporting...';
         include(dirname(__DIR__)."/html/php/ReportErrors.php");
         echo 'declaring function...';
-        function leg($text){
+        function leg($text){//Are you sure there are no issues?
             echo $text.'<br/>';
-        }/*
+        }
         echo 'Calling function...';
-        log('Handling...');
-        echo 'Whats wrong?;
+        leg('Handling...');
+        echo 'Whats wrong?';
         if(isset($_GET['pull'])){
-            log('pull is set!');
+            leg('pull is set!');
         } else {
-            log('pull is Not set :(');
-            log('');
-            log('Status:');
-            log('');
-            log(shell_exec('git status'));
+            leg('pull is Not set :(');
+            leg('');
+            leg('Status:');
+            leg('');
+            leg(shell_exec('git status'));
         }
         if (isset($_GET['pull']) && $_GET['pull']=='true') {
-            log('Pull is set to true, Pulling!');
-            log('Status before pull:');
-            log('');
-            log(shell_exec('git status'));
-            log('');
-            log('Fetching changes:');
-            log('');
-            log(shell_exec('git fetch 2>&1'));
-            log('');
-            log('Merging Changes:');
-            log('');
-            log(shell_exec('git merge 2>&1'));
-            log('');
-            log('Status after pull: ');
-            log('');
-            log(shell_exec('git status'));
+            leg('Pull is set to true, Pulling!');
+            leg('Status before pull:');
+            leg('');
+            leg(shell_exec('git status'));
+            leg('');
+            leg('Fetching changes:');
+            leg('');
+            leg(shell_exec('git fetch 2>&1'));
+            leg('');
+            leg('Merging Changes:');
+            leg('');
+            leg(shell_exec('git merge 2>&1'));
+            leg('');
+            leg('Status after pull: ');
+            leg('');
+            leg(shell_exec('git status'));
 
         }//*/
         //Pointless change to test git system
@@ -50,7 +50,7 @@
         <button id='github'>Pull</button>
         <script>
         $('#github').on('click', function(e){
-            console.log('Hreffing...');
+            console.leg('Hreffing...');
             window.location.href = 'http://jeretho.zzzz.io/github.php?pull=true';
         });
         </script>
