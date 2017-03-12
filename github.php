5abd062f991b9aa29d283912075c9fd385b1bd5f
navigatron <roboticstranger@gmail.com>
2017-03-12 17:33:06 -0400

    Outputs

--- github.php
+++ github.php
@@ -8,50 +8,64 @@
     <?php include(dirname(__DIR__)."/html/php/header.php");?>
     <div id="body" class='card'>
         <?php
-        echo 'Including error reporting...';
         include(dirname(__DIR__)."/html/php/ReportErrors.php");
-        echo 'declaring function...';
-        function leg($text){//Are you sure there are no issues?
+        leg('Error reporting is ON.');
+        function leg($text){
             echo $text.'<br/>';
         }
-        echo 'Calling function...';
-        leg('Handling...');
-        echo 'Whats wrong?';
-        if(isset($_GET['pull'])){
-            leg('pull is set!');
+        if(isset($_GET['action'])){
+            leg('Action is set!');
+            if($_GET['action']=='status'){
+                leg('Action is set to Status!');
+                leg('');
+                leg('Status before Fetch:');
+                leg('');
+                leg(shell_exec('git status'));
+                leg('');
+                leg('Fetching changes:');
+                leg('');
+                leg(shell_exec('git fetch 2>&1'));
+                leg('');
+                leg('Status after fetch: ');
+                leg('');
+                leg(shell_exec('git status'));
+            }elseif ($_GET['action']=='pull') {
+                leg('Action is set to Pull!');
+                leg('');
+                leg('Status before pull:');
+                leg('');
+                leg(shell_exec('git status'));
+                leg('');
+                leg('Fetching changes:');
+                leg('');
+                leg(shell_exec('git fetch 2>&1'));
+                leg('');
+                leg('Merging Changes:');
+                leg('');
+                leg(shell_exec('git merge 2>&1'));
+                leg('');
+                leg('Status after pull: ');
+                leg('');
+                leg(shell_exec('git status'));
+            }else{
+                leg('Action is WRONG! current setting: '.$_GET['action']);
+            }
         } else {
-            leg('pull is Not set :(');
-            leg('');
-            leg('Status:');
-            leg('');
-            leg(shell_exec('git status'));
+            leg('Action is not set!');
+            leg('Welcome to the Version control system!');
         }
-        if (isset($_GET['pull']) && $_GET['pull']=='true') {
-            leg('Pull is set to true, Pulling!');
-            leg('Status before pull:');
-            leg('');
-            leg(shell_exec('git status'));
-            leg('');
-            leg('Fetching changes:');
-            leg('');
-            leg(shell_exec('git fetch 2>&1'));
-            leg('');
-            leg('Merging Changes:');
-            leg('');
-            leg(shell_exec('git merge 2>&1'));
-            leg('');
-            leg('Status after pull: ');
-            leg('');
-            leg(shell_exec('git status'));
-
-        }//*/
         //Pointless change to test git system
          ?>
-        <button id='github'>Pull</button>
+         <button id='status'>Status</button>
+        <button id='pull'>Pull</button>
         <script>
-        $('#github').on('click', function(e){
+        $('#status').on('click', function(e){
             console.leg('Hreffing...');
-            window.location.href = 'http://jeretho.zzzz.io/github.php?pull=true';
+            window.location.href = 'http://jeretho.zzzz.io/github.php?action=status';
+        });
+        $('#pull').on('click', function(e){
+            console.leg('Hreffing...');
+            window.location.href = 'http://jeretho.zzzz.io/github.php?action=pull';
         });
         </script>
     </div>
