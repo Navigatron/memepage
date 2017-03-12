0c52b174b01c73cb97814d9c4d16177e42d777a4
navigatron <roboticstranger@gmail.com>
2017-03-12 16:19:43 -0400

    %1

--- github.php
+++ github.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 
 <head>
-    <?php include(dirname(__DIR__)."/html/php/head.php");
-    include(dirname(__DIR__)."/html/php/ReportErrors.php"); ?>
+    <?php include(dirname(__DIR__)."/html/php/head.php");?>
     <script src='js/jquery.js'></script>
 </head>
 <body>
     <div id="body">
-        <?php
-        /*
+        <?php        
+        include(dirname(__DIR__)."/html/php/ReportErrors.php");
+        echo 'About to IF';
+        if(isset(_GET['pull'])){
+            echo ', _GET[pull] is set';
+            echo ', it is equal to '._GET['pull'];
+        } else {
+            echo ', get is not set!';
+        }
         if (isset(_GET['pull']) && _GET['pull']==true) {
-            echo 'I hear you!';
+            echo ', I hear you!';
         }//*/
          ?>
         <button id='github'>Pull</button>
