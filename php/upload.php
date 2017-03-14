2c613243d485a147ff60327faa3dd2b0ff133a52
navigatron <roboticstranger@gmail.com>
2017-03-14 14:49:10 -0400

    upload js

--- php/upload.php
+++ php/upload.php
@@ -1,8 +1,12 @@
 <meta charset="utf-8">
 <?php
 //Okay, first off, wtf do we expect to be set???
-echo 'Printing keys!<br/>';
+echo 'Printing POST keys!<br/>';
 foreach (array_keys($_POST) as $key) {
+    echo 'Key: '.$key.'<br/>';
+}
+echo 'Printing FILE keys!<br/>';
+foreach (array_keys($_FILE) as $key) {
     echo 'Key: '.$key.'<br/>';
 }
 echo 'That\'s all folks';
