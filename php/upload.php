d48ca639dc5eed42d4ed53c425c76dadeaf8530d
navigatron <roboticstranger@gmail.com>
2017-03-20 18:13:55 -0400

    debuggery

--- php/upload.php
+++ php/upload.php
@@ -1,42 +1,39 @@
 <?php
 //GGRRRRRR
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-/*
-echo 'Printing ALL keys!<br/>';
-$arr = get_defined_vars();
-print_r($arr);//*/
-
+$debug = false;
 $file = $_FILES['file'];
-echo 'File is named \''.$file['name'].'\', and is '.$file['size'].' bytes in size.';
+$mimeType = mime_content_type($file['tmp_name']);
+
+if($debug){
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $arr = get_defined_vars();
+    print_r($arr);
+}
 
 //Verification
 //File MIME-type
-switch(mime_content_type($file['tmp_name'])){
+switch($mimeType){
     case 'image/jpeg':
-        echo 'Got a jpeg!';
-        break;
     case 'image/png':
-        echo 'Got a png!';
-        break;
     case 'image/gif':
-        echo 'Got a gif!';
-        break;
     case 'image/tiff':
-        echo 'Got a tiff!';
+        if($debug){
+            echo 'Got file, mimeType: '.$mimeType;
+        }
         break;
     default:
-        echo 'Not supported filetype: '+mime_content_type($file['tmp_name']);
+        echo 'Not supported filetype: '.$mimeType;
         return;
         break;
 }
 //File Size
-if($file['size'] > 4000000){
+if($file['size'] > 1000000){
     echo 'Size limit exceeded.';
     return;
+}else if($debug){
+    echo 'file size: '.$file['size'];
 }
 
 echo 'Success! Putting file in le DB!';
