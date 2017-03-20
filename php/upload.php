78cf1320f3cef68867b13a2072ab00822a3c2933
navigatron <roboticstranger@gmail.com>
2017-03-20 17:52:41 -0400

    Semicolon

--- php/upload.php
+++ php/upload.php
@@ -1,10 +1,10 @@
 <?php
 //Okay, first off, wtf do we expect to be set???
-/*
+//*
 echo 'Printing ALL keys!<br/>';
 $arr = get_defined_vars();
 print_r($arr);
-echo '<br/>'//*/
+echo '<br/>';//*/
 $file = $_FILE['file'];
 echo 'File is named \''.$file['name'].'\', and is '.$file['size'].' bytes in size.';
 
