1009c499c605884cc0b3ee021e4ebafb73f4bda1
Navigatron <roboticstranger@gmail.com>
2017-03-23 10:24:15 -0400

    Info page

--- info.php
+++ info.php
@@ -6,9 +6,18 @@
 <body>
     <!--Header-->
     <?php include(dirname(__DIR__)."/html/php/header.php");?>
-    <!--Select Memestreme-->
-    No Info here yet!
-    <!--Upload Meme-->
-    <!--Memes-->
+    <div class="card" style='text-align: left;'>
+        <p>Memestreme is a non-mainstream stream of memes, Maintained by the meme team and hosted on the meme machine.</p>
+        <p><b>MemeTeam? </b>Yeah, thats really just me right now.</p>
+        <p><b>MemeMachine?</b> More later.</p>
+        <p><b>Fresh Memes: </b>Fresh memes shows memes by freshness - most recently uploaded meme on the top.</p>
+        <p><b>Spicy Memes: </b>Memes are sorted by the number of positive votes recieved in the last time period.
+        You can change the time period and type of vote to sort by. For example, one might want to see the Dankest meme of the past two days.</p>
+        <h2>How can I help?</h2>
+        <p><b>No effort:</b> look at memes.</p>
+        <p><b>Little effort: </b>Vote on memes you like.</p>
+        <p><b>Some effort: </b>Make memes (Or steal them lol idc) and upload them.</p>
+        <p><b></b></p>
+    </div>
 </body>
 </html>
