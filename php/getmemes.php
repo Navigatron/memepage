d0c8f723c0c9ba90861c67042561bf731baf8ac5
navigatron <roboticstranger@gmail.com>
2017-03-22 21:39:02 -0400

    Error reporting to the extreme.

--- php/getmemes.php
+++ php/getmemes.php
@@ -1,6 +1,10 @@
 <?php
 define('PHPROOT', dirname(__DIR__).'/php/');
 require_once(PHPROOT.'ReportErrors.php');
+function fukitol(){
+    $arr = get_defined_vars();
+    print_r($arr);
+}
 if(!isset($_POST['index'])){
     $arr = get_defined_vars();
     print_r($arr);
@@ -17,9 +21,11 @@ if($index==0){
 require_once(PHPROOT.'ConnectToDatabase.php');
 $result = $DatabaseConnection->query($sql);
 $names = array();
+fukitol();
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {//WHERE THE KEYS FOR ROW ARE THE COLUMN NAMES
         $glob = glob(dirname(__DIR__).'/media/'.$row['id'].'.*');
+        fukitol();
         if(sizeof($glob)!=1){
             //Found more or less than one matching file.
             file_put_contents(dirname(__DIR__).'/log.txt', 'Search for file '.$row['id'].' returned '.sizeof($glob).' results.', FILE_APPEND);
@@ -35,6 +41,7 @@ if ($result->num_rows > 0) {
 }
 
 //I guess that's it.
+fukitol();
 $DatabaseConnection->close();
 return json_encode($names);
 
