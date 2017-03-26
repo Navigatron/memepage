1f9aebeeac24c583e9eaca1a4512d29d786b46cf
Navigatron <roboticstranger@gmail.com>
2017-03-26 18:33:55 -0400

    Use more document.ready

--- js/unlock.js
+++ js/unlock.js
@@ -1,37 +1,39 @@
 //We need to get the captcha code and send it to the server.
 //If the server is pleased, show the user the upload and vote features.
-$('#captchaForm').submit(function(event){
-    //Don't actually submit the form.
-    event.preventDefault();
+$(document).ready(function(){
+    $('#captchaForm').submit(function(event){
+        //Don't actually submit the form.
+        event.preventDefault();
 
-    var code = grecaptcha.getResponse();
+        var code = grecaptcha.getResponse();
 
-    if(code.length <= 0){
-        //Silly user, you need to fill out the captcha.
-        $('#captchaFeedback').val('You need to do the captcha before you click this.');
-        return false;
-    }
-
-    var formData = new FormData();
-    formData.append('code', code);
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'php/unlock.php', true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            //Post victory.
-            if(xhr.responseText=='true'){
-                //Hello human!
-                //Server has given human a cookie. Good human.
-                //Here, have some upload and vote divs.
-            }else{
-                //Go away robbit :(
-            }
-        } else {
-            alert('POST failure. Code: '+xhr.status);
+        if(code.length <= 0){
+            //Silly user, you need to fill out the captcha.
+            $('#captchaFeedback').val('You need to do the captcha before you click this.');
+            return false;
         }
-    };
-    xhr.send(formData);
 
-    //Even more not submitting the form.
-    return false;
+        var formData = new FormData();
+        formData.append('code', code);
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'php/unlock.php', true);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                //Post victory.
+                if(xhr.responseText=='true'){
+                    //Hello human!
+                    //Server has given human a cookie. Good human.
+                    //Here, have some upload and vote divs.
+                }else{
+                    //Go away robbit :(
+                }
+            } else {
+                alert('POST failure. Code: '+xhr.status);
+            }
+        };
+        xhr.send(formData);
+
+        //Even more not submitting the form.
+        return false;
+    });
 });
--- js/upload.js
+++ js/upload.js
@@ -1,40 +1,41 @@
+$(document).ready(function(){
+    $('#upload').on('click', function(e){
+        var file = $('#file')[0].files[0];
 
-$('#upload').on('click', function(e){
-    var file = $('#file')[0].files[0];
+        //TODO - clear the file button so the upload button cannot be spammed.
 
-    //TODO - clear the file button so the upload button cannot be spammed.
-
-    // Check the file type.
-    if (!file.type.match('image.*')) {
-        $('#UploadMessage').text($('#UploadMessage').text()+'That\'s Not an Image!');
-        return;
-    }
-
-    $('#UploadMessage').text('Uploading '+file.name+'... ');
-
-    var formData = new FormData();
-    formData.append('file', file, file.name);
-
-    //Getting traditional up in here
-    // Set up the request.
-    var xhr = new XMLHttpRequest();
-    // Open the connection.
-    xhr.open('POST', 'php/upload.php', true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            // File(s) uploaded.
-            //console.log('All systems go!');
-            $('#UploadMessage').text($('#UploadMessage').text()+xhr.responseText);
-            //Clear file button - taken right from stack overflow.
-            var control = $('#file');
-            control.replaceWith( control = control.clone( true ) );
-            //Clear loaded memes, re-load memes.
-            $('#memes').empty();
-            loadmemes();
-        } else {
-            alert('POST failure. Code: '+xhr.status);
-            $('#UploadMessage').text($('#UploadMessage').text()+'POST request failure.');
+        // Check the file type.
+        if (!file.type.match('image.*')) {
+            $('#UploadMessage').text($('#UploadMessage').text()+'That\'s Not an Image!');
+            return;
         }
-    };
-    xhr.send(formData);
-})
+
+        $('#UploadMessage').text('Uploading '+file.name+'... ');
+
+        var formData = new FormData();
+        formData.append('file', file, file.name);
+
+        //Getting traditional up in here
+        // Set up the request.
+        var xhr = new XMLHttpRequest();
+        // Open the connection.
+        xhr.open('POST', 'php/upload.php', true);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                // File(s) uploaded.
+                //console.log('All systems go!');
+                $('#UploadMessage').text($('#UploadMessage').text()+xhr.responseText);
+                //Clear file button - taken right from stack overflow.
+                var control = $('#file');
+                control.replaceWith( control = control.clone( true ) );
+                //Clear loaded memes, re-load memes.
+                $('#memes').empty();
+                loadmemes();
+            } else {
+                alert('POST failure. Code: '+xhr.status);
+                $('#UploadMessage').text($('#UploadMessage').text()+'POST request failure.');
+            }
+        };
+        xhr.send(formData);
+    })
+});
