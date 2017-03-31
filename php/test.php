330d59db297d2826beb9aff7e73a6e4d88ce15dc
Navigatron <roboticstranger@gmail.com>
2017-03-31 14:32:58 -0400

    Print with describe

--- php/test.php
+++ php/test.php
@@ -3,14 +3,17 @@
 if(isset($_GET['pass']) && $_GET['pass']=='8fc71f873719afaf75a9'){
     define('PHPROOT', dirname(__DIR__).'/php/');
     require_once(PHPROOT.'ConnectToDatabase.php');
-    $sql = "SELECT * FROM humans";
+    $sql = "DESCRIBE humans";
     $result = $DatabaseConnection->query($sql);
     if ($result->num_rows > 0) {
-        echo '<table><tr><td>id</td><td>token</td><td>timestamp</td></tr>';
+
+        
+        //echo '<table><tr><td>id</td><td>token</td><td>timestamp</td></tr>';
         while($row = $result->fetch_assoc()) {//WHERE THE KEYS FOR ROW ARE THE COLUMN NAMES
-            echo '<tr><td>'.$row['id'].'</td><td>'.$row['token'].'</td><td>'.$row['timestamp'].'</td></tr>';
-        }
-        echo '</table>';
+            print_r($row);
+            //echo '<tr><td>'.$row['id'].'</td><td>'.$row['token'].'</td><td>'.$row['timestamp'].'</td></tr>';
+        }/*
+        echo '</table>';//*/
     } else {
         echo "0 results";
     }
