6efb421ef63b7d89f6defd839df413c92b72353a
Navigatron <roboticstranger@gmail.com>
2017-03-31 14:34:27 -0400

    Back to select

--- php/test.php
+++ php/test.php
@@ -3,16 +3,14 @@
 if(isset($_GET['pass']) && $_GET['pass']=='8fc71f873719afaf75a9'){
     define('PHPROOT', dirname(__DIR__).'/php/');
     require_once(PHPROOT.'ConnectToDatabase.php');
-    $sql = "DESCRIBE humans";
+    $sql = "SELECT * FROM humans";
     $result = $DatabaseConnection->query($sql);
     if ($result->num_rows > 0) {
-
-        
-        //echo '<table><tr><td>id</td><td>token</td><td>timestamp</td></tr>';
+        echo '<table><tr><td>id</td><td>token</td><td>timestamp</td></tr>';
         while($row = $result->fetch_assoc()) {//WHERE THE KEYS FOR ROW ARE THE COLUMN NAMES
-            print_r($row);
-            //echo '<tr><td>'.$row['id'].'</td><td>'.$row['token'].'</td><td>'.$row['timestamp'].'</td></tr>';
-        }/*
+            //print_r($row);
+            echo '<tr><td>'.$row['id'].'</td><td>'.$row['token'].'</td><td>'.$row['time'].'</td></tr>';
+        }//*
         echo '</table>';//*/
     } else {
         echo "0 results";
