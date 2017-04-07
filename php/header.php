e1d387ad01dcc9aa4839b425054b52250b508f39
Navigatron <roboticstranger@gmail.com>
2017-04-07 13:28:41 -0400

    Info CSS and spicy memes

--- php/header.php
+++ php/header.php
@@ -1,6 +1,22 @@
 <div id="header">
 <link rel="stylesheet" type="text/css" href="css/header.css">
 <h1 id="memestreme">MEMESTREME</h1>
-<a class='navItem' href='/'>Memes</a>
+<a class='navItem' href='/'>Fresh Memes</a>
+<a class='navItem' href='/?spicy=af'>Spicy Memes</a>
 <a class='navItem' href='/info.php'>Info</a>
 </div>
+<!--
+Spicy meme arguments:
+types:
+    Spicy
+    Nice
+    Dank
+    Edgy
+    Normie
+Times:
+    fresh - 8 hours
+    some - 1 day
+    yeah - 7 days
+    totaly - 30 days
+    af - all time ever
+-->
